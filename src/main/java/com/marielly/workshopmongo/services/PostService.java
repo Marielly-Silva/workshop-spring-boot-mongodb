f20a5bc4a633634c41b9c0143df16d86ea0482dd
Marielly Silva <mariellymartinsdasilva9@gmail.com>
2025-04-29 21:36:57 -0300

    Obtendo um post por id

---
+++ src/main/java/com/marielly/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.marielly.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marielly.workshopmongo.domain.Post;
+import com.marielly.workshopmongo.repositiry.PostRepository;
+import com.marielly.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+	    Optional<Post> user = repo.findById(id);
+	    return user.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
+	}
+	
+	
+}
