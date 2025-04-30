5fc3c93d56f023f5e3b778a6db5b872b893ae13f
Marielly Silva <mariellymartinsdasilva9@gmail.com>
2025-04-30 13:04:48 -0300

    Fix: Acrescentando comentários aos posts

--- src/main/java/com/marielly/workshopmongo/domain/Post.java
+++ src/main/java/com/marielly/workshopmongo/domain/Post.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -13,9 +12,9 @@ import com.marielly.workshopmongo.dto.AuthorDTO;
 import com.marielly.workshopmongo.dto.CommentDTO;
 
 @Document
-public class Post implements Serializable{
+public class Post implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	private String id;
 	private Date date;
@@ -24,11 +23,12 @@ public class Post implements Serializable{
 	private AuthorDTO author;
 	
 	private List<CommentDTO> comments = new ArrayList<>();
-
+	
 	public Post() {
 	}
 
-	public Post(String id, Date date, String body, String title, AuthorDTO author) {
+	public Post(String id, Date date, String title, String body, AuthorDTO author) {
+		super();
 		this.id = id;
 		this.date = date;
 		this.title = title;
@@ -52,20 +52,20 @@ public class Post implements Serializable{
 		this.date = date;
 	}
 
-	public String getBody() {
-		return body;
-	}
-
-	public void setBody(String body) {
-		this.body = body;
-	}
-
 	public String getTitle() {
 		return title;
 	}
 
 	public void setTitle(String title) {
 		this.title = title;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
 	}
 
 	public AuthorDTO getAuthor() {
@@ -75,7 +75,7 @@ public class Post implements Serializable{
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
-	
+
 	public List<CommentDTO> getComments() {
 		return comments;
 	}
@@ -86,7 +86,10 @@ public class Post implements Serializable{
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -98,6 +101,11 @@ public class Post implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		Post other = (Post) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 }
--- src/main/java/com/marielly/workshopmongo/resources/util/URL.java
+++ src/main/java/com/marielly/workshopmongo/resources/util/URL.java
@@ -4,13 +4,12 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 
 public class URL {
-
-	public static String decodeParam(String text) {
-		try {
-			return URLDecoder.decode(text, "UTF-8");
-		} catch (UnsupportedEncodingException e) {
-			return " ";
-		}
-	}
-	
-}
+	 
+ 	public static String decodeParam(String text) {
+ 		try {
+ 			return URLDecoder.decode(text, "UTF-8");
+ 		} catch (UnsupportedEncodingException e) {
+ 			return "";
+ 		}
+ 	}
+ }
