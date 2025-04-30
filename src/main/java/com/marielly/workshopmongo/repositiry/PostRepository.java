3e6295af286360980579c3096db54d7f4511113b
Marielly Silva <mariellymartinsdasilva9@gmail.com>
2025-04-29 21:25:46 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/marielly/workshopmongo/repositiry/PostRepository.java
+++ src/main/java/com/marielly/workshopmongo/repositiry/PostRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.marielly.workshopmongo.domain.Post;
-import com.marielly.workshopmongo.domain.User;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
